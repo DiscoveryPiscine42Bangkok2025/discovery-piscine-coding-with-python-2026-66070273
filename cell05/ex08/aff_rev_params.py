051f5ece924acd2d5c04873398e99d6e1f711a87
66070273 <66070273@kmitl.ac.th>
2026-02-10 13:17:58 +0700

    ex8

---
+++ cell05/ex08/aff_rev_params.py
@@ -0,0 +1,13 @@
+import sys
+
+def main():
+    params = sys.argv[1:]
+    
+    if len(params) >= 2:
+        for p in reversed(params):
+            print(p)
+    else:
+        print("none")
+
+if __name__ == "__main__":
+    main()
